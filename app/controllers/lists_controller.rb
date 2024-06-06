117938f527af847a96f9ef214747a74153a0082f
yuushigure27 <yuushigure1227@gmail.com>
2024-06-06 16:19:21 +0000

    [finish]app chapter3

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,6 +13,7 @@ class ListsController < ApplicationController
   end
 
   def index
+    @lists = List.all  
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'lists/new'
   post 'lists' => 'lists#create' 
-  get 'lists/index'
+  get 'lists' => 'lists#index'
   get 'lists/show'
   get 'lists/edit'
 
