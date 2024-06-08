40529a20154d6221bbd11a2b707d3f11c1c30527
yuushigure27 <yuushigure1227@gmail.com>
2024-06-08 13:56:59 +0000

    [finish]app chapter5 edit

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   post 'lists' => 'lists#create' 
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/edit'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
 
 end
