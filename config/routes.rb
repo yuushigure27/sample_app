a9ec910f0bb68a211ffae64c9c7e826bfa881079
yuushigure27 <yuushigure1227@gmail.com>
2024-06-08 13:58:15 +0000

    [finish]app chapter5 update

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show', as: 'list'
   get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+  patch 'lists/:id' => 'lists#update', as: 'update_list'end
 
 end
